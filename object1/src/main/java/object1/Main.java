fd75cfe1d735a91bb9d6f756de6042bbb2fdaf25
Matt Kramer <matthew123003@gmail.com>
2024-02-28 15:37:19 -0500

    I changed something

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -6,5 +6,9 @@ public class Main {
 
     public static void main(String... args) {
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+
+        Person person1 = new Person("Matt",39,0);
+        System.out.println(person1.height);
+    }
 }
